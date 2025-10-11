5e9b92dad0e9ff887e01cd5fbbe7bfce90663449
Your Name <your.email@example.com>
2025-10-11 21:05:47 +0530

    deployed

--- frontend/src/utilis/config.ts
+++ frontend/src/utilis/config.ts
@@ -1,3 +1,3 @@
 
-export const BACKEND_URL = "http://localhost:3000"
+export const BACKEND_URL = "https://brainly-f9lo.onrender.com"
 export const FRONTEND_URL = "http://localhost:5173"
--- server/dist/app.js
+++ server/dist/app.js
@@ -1,13 +1,4 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -19,8 +10,8 @@ const dotenv_1 = __importDefault(require("dotenv"));
 const db_1 = __importDefault(require("./config/db"));
 const routes_1 = __importDefault(require("./routes/routes"));
 dotenv_1.default.config();
-const startServer = () => __awaiter(void 0, void 0, void 0, function* () {
-    yield (0, db_1.default)();
+const startServer = async () => {
+    await (0, db_1.default)();
     app.use((0, cors_1.default)({
         origin: ["http://localhost:5173", "http://localhost:3000"],
         methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
@@ -28,7 +19,7 @@ const startServer = () => __awaiter(void 0, void 0, void 0, function* () {
     }));
     app.use(express_1.default.json());
     app.use("/api/v1", routes_1.default);
-});
+};
 startServer();
 app.listen(process.env.PORT, () => {
     console.log("Server is runing");
