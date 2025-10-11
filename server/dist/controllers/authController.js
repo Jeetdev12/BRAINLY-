af403c99e933f8482ea1acd0eddb8c283c9bd777
Manjeet <yadumanjeet1234@gmail.com>
2025-10-11 21:05:47 +0530

    deployed

--- server/dist/controllers/authController.js
+++ server/dist/controllers/authController.js
@@ -1,13 +1,4 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -15,25 +6,25 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.signin = exports.signup = void 0;
 const userModel_1 = require("../models/userModel");
 const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
-const signup = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+const signup = async (req, res) => {
     console.log("signup route hitted..");
     try {
         const { username, email, password } = req.body;
-        const response = yield userModel_1.UserModel.create({ username, password, email });
+        const response = await userModel_1.UserModel.create({ username, password, email });
         return res.status(200).json({ message: "signedup successfully" });
     }
     catch (err) {
         console.error("❌ Error in signup:", err);
         return res.status(500).json({ error: err.message });
     }
-});
+};
 exports.signup = signup;
-const signin = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+const signin = async (req, res) => {
     {
         console.log("signin....");
         const { username, password, email } = req.body;
         try {
-            const response = yield userModel_1.UserModel.findOne({
+            const response = await userModel_1.UserModel.findOne({
                 email
             });
             if (response) {
@@ -51,5 +42,5 @@ const signin = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
             return res.status(400).json({ message: err.message });
         }
     }
-});
+};
 exports.signin = signin;
